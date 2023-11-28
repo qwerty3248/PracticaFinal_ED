0da9c6d7f5c2ba4f620591eb0de2d83d67cb20c2
qwerty3248 <jesuslosada4484@gmail.com>
2023-11-28 14:48:49 +0100

    Casi todo implementado del .cpp del almacen de rutas, quedan todos los cpps de las clases e imagen completa

--- rutas_aereas/src/AlmacenRutas.cpp
+++ rutas_aereas/src/AlmacenRutas.cpp
@@ -5,6 +5,42 @@ using namespace std;
 
 //dhacer los metodos
 
+Almacen_Rutas::Almacen_Rutas(){
+	rutas=map<string,Ruta>();
+}
+void Almacen_Rutas::Insertar(const Rutas &R){
+	rutas.insert(R);
+
+}
+void Almacen_Rutas::Borrar(const Ruta &R){
+	rutas.erase(R);
+}
+Ruta Almacen_Rutas::GetRuta(const string &a){
+	Ruta aux = rutas.find(a);
+	return aux;
+}//ni idea si está bien
+iterator Almacen_Rutas::begin(){
+	iterator it;
+	it = rutas.begin();
+	return it;
+}
+const_iterator Almacen_Rutas::begin()const{
+	const_iterator it;
+	it = rutas.begin();
+	return it;
+}
+iterator Almacen_Rutas::end(){
+	iterator it;
+	it = rutas.end();
+	return it;
+}
+const_iterator Almacen_Rutas::end()const{
+	const_iterator it;
+	it = rutas.end();
+	return it;
+}
+istream & operator >>(istream & is,Almacen_Rutas &AR);
+ostream & operator <<(ostream & os, Almacen_Rutas &R);
 
 
 
